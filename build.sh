f720c7f5c689964b63970b2383feab134396f44b
Matt Levin <matt.levin@gmail.com>
2025-08-11 21:06:33 -0400

    Update build script to find ffmpeg path

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -o errexit
 # Install ffmpeg
 apt-get update && apt-get install -y ffmpeg
 
+# Print the location of ffmpeg
+which ffmpeg
+
 # Install Python dependencies
 pip install -r storyteller-api/requirements.txt
